4fc36294fe4e969bd2f51bcf2c8bf2f1f951afcc
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2026-02-09 18:59:09 +0100

    Add first item Mod_Item

--- src/main/java/net/ra77a3l3/pocketutilities/PocketUtilities.java
+++ src/main/java/net/ra77a3l3/pocketutilities/PocketUtilities.java
@@ -1,5 +1,8 @@
 package net.ra77a3l3.pocketutilities;
 
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.ra77a3l3.pocketutilities.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -31,6 +34,8 @@ public class PocketUtilities {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -44,7 +49,9 @@ public class PocketUtilities {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.MOD_ITEM); // Now the MOD_ITEM is present in the creative tab
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/ra77a3l3/pocketutilities/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.ra77a3l3.pocketutilities.item;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.ra77a3l3.pocketutilities.PocketUtilities;
+
+public class ModItems {
+
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(PocketUtilities.MOD_ID);
+
+    public static final DeferredItem<Item> MOD_ITEM = ITEMS.register("mod_item",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
