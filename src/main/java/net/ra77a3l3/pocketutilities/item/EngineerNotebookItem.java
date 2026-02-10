0b1974dd32b6ff910d73c5218290badd606f1ff3
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2026-02-10 09:40:21 +0100

    Add tooltip to Engineer's Notebook

--- src/main/java/net/ra77a3l3/pocketutilities/item/EngineerNotebookItem.java
+++ src/main/java/net/ra77a3l3/pocketutilities/item/EngineerNotebookItem.java
@@ -1,13 +1,16 @@
 package net.ra77a3l3.pocketutilities.item;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class EngineerNotebookItem extends Item {
 
@@ -27,5 +30,13 @@ public class EngineerNotebookItem extends Item {
         }
 
         return InteractionResultHolder.success(itemstack);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        tooltipComponents.add(Component.literal("Your guide to portable engineering"));
+        tooltipComponents.add(Component.literal("Right-click to read").withStyle(ChatFormatting.GRAY));
+
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
